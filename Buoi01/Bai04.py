b0faee4aaad70de41f57a9a7361153f638c216ca
Le Kiet <kiethohohoho@gmail.com>
2023-08-26 10:01:58 +0700

    update bai04

--- Buoi01/Bai04.py
+++ Buoi01/Bai04.py
@@ -1,10 +1,9 @@
 year = input("What year is it?\n")
-
-print(f"""
-                                     .   :   .
-                                 ' .     :     . '
-        HAPPY NEW YEAR          .    ' . : . '    .
-        !!!  {year}  !!!          .    . ' : ' .    .
-                                 . '     :     ' .
-                                     '   :   '
+print(f"""        
+                      . : .
+                   '.   :   .'
+HAPPY NEW YEAR    .   '.:.'   .
+!!!  {year}  !!!    .   .':'.   .
+                   .'   :   '.
+                      ' : '
 """)

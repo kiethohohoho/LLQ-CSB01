5c2d4ed00a340a36f6b763da063d71d9987bd67c
Le Kiet <kiethohohoho@gmail.com>
2023-09-24 15:58:02 +0700

    Improve code clean

--- Buoi05 - CheckPoint 01/Part6/Bai1.py
+++ Buoi05 - CheckPoint 01/Part6/Bai1.py
@@ -2,14 +2,12 @@
 def check_odd_digit(n):
     n = str(n)
 
-    even_count = 0
-
     for digit in n:
         if int(digit) % 2 == 0:
-            even_count += 1
+            return True
 
 
-    return even_count > 0
+    return False
 
 n = int(input("Input a number: "))
 

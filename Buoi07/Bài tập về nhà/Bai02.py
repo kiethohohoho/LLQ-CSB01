e066b2fdce837bc3902a7a212d50b2d8978cd869
Le Kiet <kiethohohoho@gmail.com>
2023-10-08 08:28:21 +0700

    update bai2 buoi 7

--- Buoi07/Bài tập về nhà/Bai02.py
+++ Buoi07/Bài tập về nhà/Bai02.py
@@ -1,4 +1,4 @@
 arr = ['l', 'o', 'o', 'h', 'c', 'S', ' ', 'y', 'g', 'o', 'l', 'o', 'n', 'h', 'c', 'e', 'T', ' ', 'X', 'd', 'n', 'i', 'M']
 
-original_string = ' '.join(''.join(arr).split(' '))
+original_string = ''.join(arr[::-1])
 print("Chuỗi ban đầu:", original_string)
